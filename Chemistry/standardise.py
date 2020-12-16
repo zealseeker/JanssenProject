37fcf5f5f44d8e111c6a0c6b6b1b6b8cc49a213b
Hongbin Yang <yanyanghong@163.com>
2020-12-16 16:55:48 +0000

    Add standardisation

---
+++ Chemistry/standardise.py
@@ -0,0 +1,29 @@
+import warnings
+
+from rdkit import Chem
+import pandas as pd
+from chembl_structure_pipeline import standardizer
+import tqdm
+
+def read_standardise(smi) -> Chem.rdchem.Mol:
+    null_mol = Chem.MolFromSmiles('')
+    try:
+        m = Chem.MolFromSmiles(smi)
+        assert m is not None
+    except:
+        warnings.warn('SMILES: {} is invalid'.format(smi))
+        return null_mol
+    m = standardizer.standardize_mol(m)
+    m = standardizer.get_parent_mol(m)[0]
+    return m
+
+def standardise_dataframe(df):
+    mols = df['Compound SMILES'].apply(read_standardise)
+    df['std_canonical_smiles'] = mols.apply(Chem.MolToSmiles)
+    df['std_inchi_key'] = mols.apply(Chem.MolToInchiKey)
+    return df
+
+if __name__ == "__main__":
+    df = pd.read_csv('../LINCS/input/janssen_compounds.csv')
+    df = standardise_dataframe(df)
+    df.to_csv('janssen_compounds_std.csv')
--- README.md
+++ README.md
@@ -8,3 +8,10 @@ TO DO:
 
 Please create your own branch and submit a pull request, any modifications to code can then be merged with the main branch after code review by other collaborators.
 https://dont-be-afraid-to-commit.readthedocs.io/en/latest/git/commandlinegit.html
+
+### Requirements
+
+[ChEMBL Structure Pipeline](https://github.com/chembl/ChEMBL_Structure_Pipeline) is required to standarise the molecules
+```
+conda install -c chembl chembl_structure_pipeline
+```
